e56cf54bf96330319a6507f41e888c2a5dc0d91d
William Arild Dahl <williada@sejer.ifi.uio.no>
2017-09-21 12:39:19 +0200

    Added AspWhileStmt

---
+++ inf2100/no/uio/ifi/asp/parser/AspWhileStmt.java
@@ -0,0 +1,19 @@
+class AspWhileStmt extends AspStmt {
+  AspExpr test;
+  AspSuite body;
+
+  AspWhileStmt(int n) {
+    super(n);
+  }
+
+
+  static AspWhileStmt parse(Scanner s) {
+
+    Main.log.enterParser("while stmt");
+    AspWhileStmt aws = new AspWhileStmt(s.curLineNum());
+    skip(s, whileToken);  aws.test = AspExpr.parse(s);
+    skip(s, colonToken);  aws.body = AspSuite.parse(s);
+
+    Main.log.leaveParser("while stmt");
+    return aws;
+}
