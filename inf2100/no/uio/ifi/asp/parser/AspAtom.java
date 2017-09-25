39c34c60d9473efcb0c4e0eb468e8ac6ef8af254
William Arild Dahl <williada@norge.ifi.uio.no>
2017-09-25 13:30:29 +0200

    Atom done

--- inf2100/no/uio/ifi/asp/parser/AspAtom.java
+++ inf2100/no/uio/ifi/asp/parser/AspAtom.java
@@ -1,14 +1,55 @@
+import static no.uio.ifi.asp.scanner.TokenKind.*;
 class AspAtom extends AspSyntax{
+	AspInnerExpr body1;
+	AspDictDisplay body2;
+	AspListDisplay body3;
+
 	AspAtom(int n) {
 		super(n);
 	}
 
 	static AspAtom parse(Scanner s){
+		AspAtom aat = new AspAtom(s.curLineNum());
+
 		Main.log.enterParser("atom");
-		if()
-
-
+		if(s.curToken().kind == nameToken){
+				Main.log.enterParser("name");
+				Main.log.leaveParser("name");
+				skip(s, nameToken);
+		}else if(s.curToken().kind == integerToken){
+			Main.log.enterParser("integer literal");
+			Main.log.leaveParser("integer literal");
+			skip(s, integerToken);
+		}else if(s.curToken().kind == floatToken){
+			Main.log.enterParser("float literal");
+			Main.log.leaveParser("float literal");
+			skip(s, floatToken);
+		}else if(s.curToken.kind == stringToken){
+			Main.log.enterParser("string literal");
+			Main.log.leaveParser("string literal");
+			skip(s, stringToken);
+		}else if((s.curToken.kind == falseToken) || (s.curToken.kind == falseToken) ){
+			Main.log.enterParser("boolean literal");
+			Main.log.leaveParser("boolean literal");
+			skip(s, s.curToken.kind);
+		}else if((s.curToken.kind == noneToken ){
+			Main.log.enterParser("none literal");
+			Main.log.leaveParser("none literal");
+			skip(s, noneToken);
+		}else if(s.curToken.toString().charAt(0) == '('){
+			skip(s, leftParToken);
+			aat.body1 = AspInnerExpr.parse(s);
+		}else if(s.curToken.toString().charAt(0) == '['){
+			skip(s, leftBracketToken);
+			aat.body3 = AspListDisplay.parse(s);
+		}else if(s.curToken.toString().charAt(0) == '{'){
+			skip(s, leftBraceToken);
+			aat.body2 = AspListDisplay.parse(s);
+		}else{
+			parserError("no elem ",s.curLineNum());
+		}
+		Main.log.leaveParser("atom");
+		return aat;
 	}
-
 
 }
--- inf2100/no/uio/ifi/asp/parser/AspFactorPrefix.java
+++ inf2100/no/uio/ifi/asp/parser/AspFactorPrefix.java
@@ -2,4 +2,12 @@ class AspFactorPrefix extends AspSyntax{
 	AspFactorPrefix(int n){
 		super(n)
 	}
+
+	static AspFactorPrefix parse(Scanner s){
+		AspFactorPrefix afp = new AspFactorPrefix(s.curLineNum());
+		Main.log.enterParser("factor prefix");
+		Main.log.leaveParser("factor prefix");
+		return afp;
+
+	}
 }
